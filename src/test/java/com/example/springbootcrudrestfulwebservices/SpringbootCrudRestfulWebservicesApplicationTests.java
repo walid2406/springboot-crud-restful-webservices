46615436a08adb8c9189c89b6b360e598c42b0a0
walid.abboud.tech <walid.abboud.tech@gmail.com>
2021-12-14 15:29:54 +0100

    version  selon tuto rest a verifier jdk

---
+++ src/test/java/com/example/springbootcrudrestfulwebservices/SpringbootCrudRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcrudrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
